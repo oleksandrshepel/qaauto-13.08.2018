300dfbc021329c4dd23bbdea5b84a1f0354ae81d
oleksandrshepel <oleksshepel@gmail.com>
2018-09-17 10:58:35 +0300

    HomeAssignment 8

--- src/test/java/page/LinkedinBasePage.java
+++ src/test/java/page/LinkedinBasePage.java
@@ -1,17 +1,45 @@
 package page;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import util.GMailService;
 
+/**
+ * Linkedin parent object class
+ *
+ * Contains declaration of WebDriver instance, WebDriverWait instance and create gMailService instance
+ */
 public class LinkedinBasePage {
     protected WebDriver driver;
     protected WebDriverWait wait;
+    protected static GMailService gMailService = new GMailService();
 
-    public String getCurrentUrl(){
+    /**
+     * Common method to get current url of an appropriate pageobject class
+     * @return - current url
+     */
+    protected String getCurrentUrl(){
         return driver.getCurrentUrl();
     }
 
-    public String getCurrentTitle(){
+    /**
+     * Common method to get current title of an appropriate pageobject class
+     * @return - current title
+     */
+    protected String getCurrentTitle(){
         return driver.getTitle();
+    }
+
+    /**
+     Base method for waiting  webelement visibility
+     * @param webElement
+     * @param timeOutInSeconds
+     * @return - webelement
+     */
+    protected WebElement waitUntilElementVisible(WebElement webElement, int timeOutInSeconds){
+        WebDriverWait wait = new WebDriverWait(driver, timeOutInSeconds);
+        return wait.until(ExpectedConditions.visibilityOf(webElement));
     }
 }
