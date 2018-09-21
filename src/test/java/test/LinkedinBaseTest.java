4474403335e065d471a58aeb8e034e5a74bdb61e
oleksandrshepel <oleksshepel@gmail.com>
2018-09-21 14:44:49 +0300

    Lesson 11

--- src/test/java/test/LinkedinBaseTest.java
+++ src/test/java/test/LinkedinBaseTest.java
@@ -9,7 +9,6 @@ import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Optional;
-//import org.testng.annotations.Parameters;
 import org.testng.annotations.Parameters;
 import page.*;
 
